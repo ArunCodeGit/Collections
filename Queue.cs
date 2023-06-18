efd84206c29d96bc23dcd67145c28b4bcebf9f39
Arun Kumar <76198217+ArunCodeGit@users.noreply.github.com>
2023-06-18 22:51:01 +0530

    Add files via upload

---
+++ Queue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Queue
+    {
+        public static void MyQueue()
+        {
+            //A queue can be defined as an ordered list which enables insert operations to be performed at one end called REAR and
+            //delete operations to be performed at another end called FRONT.
+
+
+            Queue<string> mq = new Queue<string>();
+            mq.Enqueue("Arun");
+            mq.Enqueue("Kumar");
+
+            mq.Enqueue("Murali");
+            mq.Enqueue("Dharan");
+
+            Console.WriteLine(mq.Dequeue());
+            Console.WriteLine(mq.Dequeue());
+            Console.WriteLine(mq.Peek() + " is peek view");
+
+            while (mq.Count > 0)
+            {
+                Console.WriteLine(mq.Dequeue());
+            }
+        }
+    }
+}
